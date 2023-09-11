76cc26dc8414a9844fb433204420df1bba61437f
Niklas Horn <nijoergensen@gmail.com>
2023-09-12 00:26:38 +0200

    stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def button(text, path)
+        link_to text, path, class: "bg-slate-400 hover:bg-slate-500 text-white rounded-lg transition duration-300 ease-in-out px-4 py-2"
+    end
 end
